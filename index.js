6d2c96b4648b5d442d2394930cbf37b75096653d
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-05-13 18:13:17 -0600

    Move more css and js to own files

--- index.js
+++ index.js
@@ -4,62 +4,89 @@ var theCanvas;
 var xLast = null;
 var yLast = null;
 var drawMode = null;
+var select_width;
+var select_color;
+var select_drawmode;
 
-function init(){
-  var select_width = document.getElementById('select_width');
-  var select_color = document.getElementById('select_color');
-  var select_drawmode = document.getElementById('select_drawmode');
+window.addEventListener('load', init);
 
+function init() {
+  // Store references to UI elements
+  select_width = document.getElementById('select_width');
+  select_color = document.getElementById('select_color');
+  select_drawmode = document.getElementById('select_drawmode');
   theCanvas = document.getElementById('theCanvas');
   context = theCanvas.getContext("2d");
-  context.strokeStyle = select_color.options[select_color.selectedIndex].value;
-  context.fillStyle = select_color.options[select_color.selectedIndex].value;
-  context.lineWidth = select_width.options[select_width.selectedIndex].value;
   
-  drawMode = select_drawmode.options[select_drawmode.selectedIndex].value;
+  // Set defaults
+  setStrokeStyle();
+  setFillStyle();
+  setLineWidth();
+  setDrawMode();
+
+  // Add event listeners
+  theCanvas.addEventListener('mousedown', mouseDown);
+  theCanvas.addEventListener('mousemove', mouseMove);
+  window.addEventListener('mouseup', mouseUp);
 }
 
-function mouseDown(ev){
+function mouseDown(ev) {
   mouseIsPressed = true;
 }
 
-function mouseUp(ev){
+function mouseUp(ev) {
   mouseIsPressed = false;
   xLast = null;
   yLast = null;
 }
 
-function mouseMove(ev){
-  if(mouseIsPressed){
+function mouseMove(ev) {
+  if (mouseIsPressed) {
     xNow = ev.clientX - theCanvas.offsetLeft;
     yNow = ev.clientY - theCanvas.offsetTop;
-    
-    
-    if(xLast != null){
-      
-      if(drawMode == "stroke"){
+
+
+    if (xLast != null) {
+
+      if (drawMode == "stroke") {
         context.beginPath();
-        context.moveTo(xLast,yLast);
+        context.moveTo(xLast, yLast);
         context.lineTo(xNow, yNow);
         context.stroke();
-      } else if(drawMode == "fill"){
+      } else if (drawMode == "fill") {
         context.beginPath();
-        context.moveTo(xLast,yLast);
-        context.lineTo(xLast + (context.lineWidth/2),yLast + (context.lineWidth/2));
-        context.lineTo(xNow + (context.lineWidth/2), yNow + (context.lineWidth/2));
+        context.moveTo(xLast, yLast);
+        context.lineTo(xLast + (context.lineWidth / 2), yLast + (context.lineWidth / 2));
+        context.lineTo(xNow + (context.lineWidth / 2), yNow + (context.lineWidth / 2));
         context.lineTo(xNow, yNow);
         context.lineTo(xLast, yLast);
         context.fill();
       }
     }
-    
+
     xLast = xNow;
     yLast = yNow;
   }
 }
 
-function erase(){
-  context.fillStyle='rgb(255,255,255)';
-  context.fillRect(0,0,1000,500);
+function setStrokeStyle() {
+  context.strokeStyle = select_color.options[select_color.selectedIndex].value;
+}
+
+function setFillStyle() {
+  context.fillStyle = select_color.options[select_color.selectedIndex].value;
+}
+
+function setLineWidth() {
+  context.lineWidth = select_width.options[select_width.selectedIndex].value;
+}
+
+function setDrawMode(){
+  drawMode = select_drawmode.options[select_drawmode.selectedIndex].value;
+}
+
+function erase() {
+  context.fillStyle = 'rgb(255,255,255)';
+  context.fillRect(0, 0, 1000, 500);
   init();
 }
